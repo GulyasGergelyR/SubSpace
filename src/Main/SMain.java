f0a0b045da051aa67fe792b33bf32e402a1bebff
Unknown <g.gergo42@gmail.com>
2016-10-21 18:00:47 +0200

    Client can create window

--- src/Main/SMain.java
+++ src/Main/SMain.java
@@ -2,13 +2,20 @@ package Main;
 
 import javax.swing.JOptionPane;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
 import GameEngine.SGameInstance;
+import RenderingEngine.SRenderer;
 import WebEngine.Client;
 import WebEngine.Server;
 
 public class SMain {
 	
 	private static SGameInstance gameInstance;
+	private static SRenderer renderer;
 	private static int delta;
 	
 	public static void main(String[] args) {
@@ -43,19 +50,31 @@ public class SMain {
 	}
 	private static void InitClient(){
 		gameInstance = new SGameInstance();
+		renderer = new SRenderer(gameInstance);
+		
+		try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+ 
+        initGL(); // init OpenGL
 	}
 	
 	private static void StartServer(){
 		
 	}
 	private static void StartClient(){
-		while(true){
+		while (!Display.isCloseRequested()) {
 			System.out.println(getDelta()+" "+getDeltaRatio());
-			for(long i=0;i<100000000;i++){
-				
-			}
+			renderer.DrawObjects();
+			Display.update();
+            Display.sync(60); // cap fps to 60fps
 			updateDelta();
 		}
+		Display.destroy();
 	}
 
 	
@@ -74,4 +93,11 @@ public class SMain {
 	public static float getDeltaRatio(){
 		return ((float)delta)/gameInstance.getFPS().getFPS_M();
 	}
+	
+	public static void initGL() {
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, 800, 0, 600, 1, -1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+    }
 }
